74e451ac97efa8216c2f758f0a643a52570e4e2c
kvaldesvallejo <kvaldesvallejo@gmail.com>
2020-04-14 21:32:54 +0000

    Editar contacto

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,4 +1,4 @@
-const url = "https://assets.breatheco.de/apis/fake/contact/agenda/kAgenda";
+const url = "https://assets.breatheco.de/apis/fake/contact/";
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
@@ -9,12 +9,60 @@ const getState = ({ getStore, setStore }) => {
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
 			loadContact() {
-				fetch(url)
+				fetch(url + "agenda/kAgenda")
 					.then(response => response.json())
 					.then(result => {
 						console.log("Get contact: ", result), setStore({ contacts: result });
 					})
 					.catch(e => console.error(e));
+			},
+			addContact(name, phone, email, address) {
+				fetch(url, {
+					method: "post",
+					headers: { "Content-type": "application/json" },
+					body: JSON.stringify({
+						full_name: name,
+						phone: phone,
+						address: address,
+						email: email,
+						agenda_slug: "kAgenda"
+					})
+				}).then(() => {
+					fetch(url)
+						.then(response => response.json())
+						.then(result => {
+							console.log("result: ", result);
+							setStore({
+								contacts: result
+							});
+						})
+						.catch(e => console.error(e));
+				});
+			},
+			editContact(id, name, phone, email, address) {
+				fetch(url + id, {
+					method: "put",
+					headers: { "Content-type": "application/json" },
+					body: JSON.stringify({
+						full_name: name,
+						phone: phone,
+						address: address,
+						agenda_slug: "kAgenda"
+					})
+				}).then(() => {
+					fetch(
+						url +
+							"agenda/kAgenda"
+								.then((response = response.json()))
+								.then(response => {
+									console.log("result: ", result),
+										setStore({
+											contacts: result
+										});
+								})
+								.catch(e => console.error(e))
+					);
+				});
 			}
 		}
 	};
